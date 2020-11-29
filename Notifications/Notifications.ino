2ce298aa7a895ed82627b12c58de0708c3c8afa5
john bargman <darthpjb@gmail.com>
2020-11-29 03:10:16 +0000

    updated notifier code to correctly act with a blinking light and two timers

--- Notifications/Notifications.ino
+++ Notifications/Notifications.ino
@@ -6,33 +6,91 @@
 #include <Ticker.h>
 
 #define LED_BUILTIN 2
-const String github_token = "...";
+const String github_token = "7a17d94e72afdb5699d925c723f82c4130c9de87";
 
 const char* host = "api.github.com";
 const int httpsPort = 443;
 
 Ticker checker;
+Ticker checker2;
 
+bool On_Off = false;
+bool Has_Notifications = false;
 bool check_now = false;
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.begin(115200);
-  WiFiManager wifiManager;
-  wifiManager.autoConnect("AutoConnectAP");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  mark_to_be_checked();
-  checker.attach(60, mark_to_be_checked);
+const char* root_ca= \
+     "-----BEGIN CERTIFICATE-----\n" \
+     "MIIEkjCCA3qgAwIBAgIQCgFBQgAAAVOFc2oLheynCDANBgkqhkiG9w0BAQsFADA/\n" \
+     "MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \
+     "DkRTVCBSb290IENBIFgzMB4XDTE2MDMxNzE2NDA0NloXDTIxMDMxNzE2NDA0Nlow\n" \
+     "SjELMAkGA1UEBhMCVVMxFjAUBgNVBAoTDUxldCdzIEVuY3J5cHQxIzAhBgNVBAMT\n" \
+     "GkxldCdzIEVuY3J5cHQgQXV0aG9yaXR5IFgzMIIBIjANBgkqhkiG9w0BAQEFAAOC\n" \
+     "AQ8AMIIBCgKCAQEAnNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EF\n" \
+     "q6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8\n" \
+     "SMx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0\n" \
+     "Z8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWA\n" \
+     "a6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj\n" \
+     "/PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3DkwIDAQABo4IBfTCCAXkwEgYDVR0T\n" \
+     "AQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwfwYIKwYBBQUHAQEEczBxMDIG\n" \
+     "CCsGAQUFBzABhiZodHRwOi8vaXNyZy50cnVzdGlkLm9jc3AuaWRlbnRydXN0LmNv\n" \
+     "bTA7BggrBgEFBQcwAoYvaHR0cDovL2FwcHMuaWRlbnRydXN0LmNvbS9yb290cy9k\n" \
+     "c3Ryb290Y2F4My5wN2MwHwYDVR0jBBgwFoAUxKexpHsscfrb4UuQdf/EFWCFiRAw\n" \
+     "VAYDVR0gBE0wSzAIBgZngQwBAgEwPwYLKwYBBAGC3xMBAQEwMDAuBggrBgEFBQcC\n" \
+     "ARYiaHR0cDovL2Nwcy5yb290LXgxLmxldHNlbmNyeXB0Lm9yZzA8BgNVHR8ENTAz\n" \
+     "MDGgL6AthitodHRwOi8vY3JsLmlkZW50cnVzdC5jb20vRFNUUk9PVENBWDNDUkwu\n" \
+     "Y3JsMB0GA1UdDgQWBBSoSmpjBH3duubRObemRWXv86jsoTANBgkqhkiG9w0BAQsF\n" \
+     "AAOCAQEA3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJo\n" \
+     "uM2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/\n" \
+     "wApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwu\n" \
+     "X4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlG\n" \
+     "PfZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6\n" \
+     "KOqkqm57TH2H3eDJAkSnh6/DNFu0Qg==\n" \
+     "-----END CERTIFICATE-----\n";
+
+void mark_to_be_checked();
+void blink_led();
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.begin(115200);
+    WiFiManager wifiManager;
+    wifiManager.autoConnect("AutoConnectAP");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    mark_to_be_checked();
+    checker.attach(10, mark_to_be_checked);
+    checker2.attach(1,blink_led);
 }
 
-
-void mark_to_be_checked() {
+void blink_led()
+{
+  if(Has_Notifications )
+  {
+    if(On_Off)   
+    {
+      digitalWrite(LED_BUILTIN, LOW);
+      On_Off = false;
+    }
+    else   
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+      On_Off = true;
+    }
+  }
+}
+void mark_to_be_checked() 
+{
   check_now = true;
 }
-void check() {
+
+void check() 
+{
   WiFiClientSecure client;
+
+  //TODO : Nixinator, look here
+  //client.setCACert(root_ca);
   Serial.print("connecting to ");
   Serial.println(host);
   if (!client.connect(host, httpsPort)) {
@@ -49,14 +107,19 @@ void check() {
   Serial.println("GET request sent");
   while (client.connected()) {
     String line = client.readStringUntil('\n');
-    if (line.startsWith("Content-Length:")) {
+    //Serial.println(line);
+    if (line.startsWith("Content-Length:")) 
+    {
       Serial.println(line.substring(16));
       int length = line.substring(16).toInt();
-      if (length > 10) {
+      if (length > 10) 
+      {
         Serial.println("We have notifications");
-        digitalWrite(LED_BUILTIN, LOW);
-      } else {
-        digitalWrite(LED_BUILTIN, HIGH);
+        Has_Notifications = true;
+      } 
+      else 
+      {
+        Has_Notifications = false;
       }
     }
     if (line == "\r") {
@@ -68,8 +131,10 @@ void check() {
   client.stop();
 }
 
-void loop() {
-  if (check_now == true) {
+void loop() 
+{
+  if (check_now == true)
+   {
     check();
     check_now = false;
   }
